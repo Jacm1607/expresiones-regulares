e14acec9264cc3b9cb9af40e348a5851086a26d5
Jose Alfredo Chavarria <46409841+Jacm1607@users.noreply.github.com>
2022-03-31 09:05:50 -0400

    Primera definiciones

---
+++ example.js
@@ -0,0 +1,7 @@
+const expresiones = {
+  correo : /^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$/, // Solo correo
+  numero : /^\d{3,6}$/, // Solo numeros de 3 a 6 caracter
+  password : /^[a-zA-Z0-9*_.]{8}$/, // Password de 8 caracteres, acepta solo * _ .
+  texto : /^[a-zA-Z ]{2,6}/ // Solo texto de 2 a 6 caracteres
+  decimales: /[0-9]+([.][0-9]+)?$/ 
+}
